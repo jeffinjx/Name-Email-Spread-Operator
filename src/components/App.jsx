ed3b9bcf8ecd424b3d55ea9e42c9638f1d437b77
Jeffin Jaison <jeffinjaison.9@gmail.com>
2022-12-18 19:26:47 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function App() {
+  const [contact, setContact] = useState({
+    fName: "",
+    lName: "",
+    email: ""
+  });
+
+  function handleChange(e) {
+
+    const { name, value } = e.target;
+
+    setContact((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value
+      };
+    });
+  }
+
+  function handleClick(e){
+    e.persist();
+  }
+
+  return (
+    <div className="container">
+      <h1>
+        Hello {contact.fName} {contact.lName}
+      </h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button onClick={handleClick}>Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
